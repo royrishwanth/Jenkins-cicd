9b4b57e8e10197a2022d468ce65299ba9166931b
Rishwanth Kothapalli <rishwanthkothapalli@Apples-MacBook-Air.local>
2025-07-20 16:16:59 +0530

    Java app and jenkinsfile with k8s manifest

---
+++ ultimate-pipeline/java/src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("🚀 Hello World from a Java 17 application!");
+        System.out.println("✅ Built with Maven");
+        System.out.println("🔁 Running through a CI/CD pipeline!");
+        System.out.println("📦 Ready to ship!");
+    }
+}
